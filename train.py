009b9bdfd94ac70f2e2346716cb52ed2ad0cebf1
Kentaro Machida <kenkenm120319@gmail.com>
2025-01-06 23:31:32 +0900

    MLflowが動いた

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import pytorch_lightning as pl
 import yaml
-from src.pkgs.data_classes.config_class import ExperimentConfig, TrainConfig
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning.loggers import MLFlowLogger
+
+from src.pkgs.data_classes.config_class import ExperimentConfig, TrainConfig, MLflowConfig
 from src.pkgs.model_datasets.model_dataset_factory import ModelDatasetFactory
 
 def main():
@@ -20,9 +23,35 @@ def main():
     train_loader = model_dataset_dict["train_loader"]
     val_loader = model_dataset_dict["val_loader"]
 
+    # Callbacks
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_loss",  # Metric to monitor
+        dirpath="checkpoints/",  # Directory to save checkpoints
+        filename="best-checkpoint-{epoch:02d}-{val_loss:.2f}",
+        save_top_k=1,  # Save only the best model
+        mode="min"  # Minimize the monitored metric
+    )
+
+    early_stopping_callback = EarlyStopping(
+        monitor="val_loss",
+        patience=5,  # Number of epochs to wait before stopping
+        mode="min"
+    )
+
+    mlflow_config = MLflowConfig.model_validate(config["experiment"]["mlflow_config"])
+    logger = MLFlowLogger(
+        experiment_name=mlflow_config.experiment_name,
+        tracking_uri=mlflow_config.tracking_uri
+    )
+    logger.log_hyperparams(config["experiment"])
+
     trainer = pl.Trainer(
         max_epochs=train_config.max_epochs,
-        accelerator='cpu'
+        accelerator='cpu',
+        devices=1,
+        callbacks=[checkpoint_callback, early_stopping_callback],
+        logger=logger,
+        log_every_n_steps=10
     )
     trainer.fit(model, train_loader, val_loader)
 
